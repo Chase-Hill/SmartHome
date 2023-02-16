31f0aee489006dc56296c8faf53b1092d9c92a9b
Chase <chaseaohill@gmail.com>
2023-02-16 15:36:06 -0500

    Swapping branches

---
+++ SmartHome/Controllers/View Controller/ToggleAllViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleAllViewController.swift
+//  SmartHome
+//
+//  Created by Chase on 2/16/23.
+//
+
+import UIKit
+
+class ToggleAllViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SmartHome/Resources/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  SmartHome
+//
+//  Created by Chase on 2/16/23.
+//
+
+import Foundation
+
